237f1f361e420533e85102a8dbd014519fbfd933
800076254 <800076254@CTXVDG1059>
2025-10-18 01:14:23 -0500

    added array to store values, added validation methods, and added logic to setting user's choice

--- CPP-TicTacToe/TicTacToe.cpp
+++ CPP-TicTacToe/TicTacToe.cpp
@@ -3,25 +3,61 @@
 
 #include <iostream>
 
+
 void TicTacToe::Display() const
 {
-	std::cout << " 1|2|3\n -----\n 4|5|6\n -----\n 7|8|9";
+	std::cout << m_board[0]<<"|" << m_board[1] << "|" << m_board[2] << "\n" << "-----\n"
+		<< m_board[3] << "|" << m_board[4] << "|" << m_board[5] << "\n" << "-----\n"
+		<< m_board[6] << "|" << m_board[7] << "|" << m_board[8] << "\n";
 }
 
 bool TicTacToe::IsGameOver() const
 {
-	//what loop is this =9 (tie)
-	if()
+	//Work on this
+	if(isWinner() || GameCount = 9)
 	return false;
 }
 
 void TicTacToe::TakeTurn()
 {
+	int Player = GetPlayer();
+	int choice = 0;
+	bool isValid = false;
+
+	//determine who's turn it is
+	Player == 0 ? Player = 1 : Player = 0;
+	
+	//get the user's choice
+	while (!(isValid))
+	{
+		std::cout << "Player " << (Player + 1) << ", pick a number between 1-9\n";
+		std::cin >> choice;
+
+		isValid = ValidateChoice(choice);
+		if (!(isValid))
+		{
+			std::cout << "Please enter a valid spot\n";
+		}
+	}
+
+	if (Player == 0)
+	{
+		SetArray('X', choice);
+	}
+	else
+	{
+		SetArray('O', choice);
+
+	}
+
+	SetPlayer(Player);
 
 }
 
 bool isWinner()
 {
-	bool isWinner = false;
-	if()
+	
+	
+
+	return false;
 }
--- CPP-TicTacToe/TicTacToe.h
+++ CPP-TicTacToe/TicTacToe.h
@@ -4,12 +4,51 @@
 
 class TicTacToe : public Game
 {
+private:
+	char m_board[9] = { '1','2','3','4','5','6','7','8','9'};
+	int Player = 1;
+	int GameCount = 1;
 public:
 	TicTacToe() {};
 	~TicTacToe() {};
 
 	// Methods to be implemented by derived class
+	void SetArray(const char v, const int choice)
+	{
+				m_board[(choice - 1)] = v;
+	}
+	void SetPlayer(const int i)
+	{
+		Player = i;
+	}
+	int GetPlayer()const {
+		return Player;
+	}
+	void SetGameCount(const int i)
+	{
+		GameCount = i;
+	}
+	int GetGameCount()const {
+		return GameCount;
+	}
+
 	bool IsGameOver() const;
 	void TakeTurn();
 	void Display() const;
+	bool ValidateChoice(const int i)
+	{
+		//validate that choice is in the correct range
+		if((i < 1) && (i > 9))
+		{
+			return false;
+		}
+		//validate that the choice is not already taken
+		if (!(m_board[(i-1)] == 'X') && !(m_board[(i-1)] == 'O'))
+		{
+			
+			return true;
+		}
+
+		return false;
+	}
 };
